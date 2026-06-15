c14473d61886bfae4cbe175756e4aff34d27e7aa
Amanda <amanda.simmons137@gmail.com>
2026-06-15 19:54:26 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("Hello, World!");
+
+
+let item1 = 5; //Price of item 1
+let item2 = Number("10"); // Convert string to a number
+
+let total = item1 + item2;
+console.log("Total price:", total);
